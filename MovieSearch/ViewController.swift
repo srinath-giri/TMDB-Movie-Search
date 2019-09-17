dbf1a5e0777bcea3b3d4977362d35e2caa029154
Srinath Giri <srinathcbe43@gmail.com>
2019-09-16 19:27:32 -0700

    Initial Commit

---
+++ MovieSearch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieSearch
+//
+//  Created by Srinath Giri on 9/16/19.
+//  Copyright © 2019 Example. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
